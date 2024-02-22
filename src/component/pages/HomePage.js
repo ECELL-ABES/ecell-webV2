1bb646ba5e9547f537afad426edfd24394cefd76
Abhay <a.thakur01111@gmail.com>
2024-02-22 22:26:03 +0530

    achievement section updated

--- src/component/pages/HomePage.js
+++ src/component/pages/HomePage.js
@@ -1,27 +1,25 @@
-import React, {useState, useEffect} from 'react'
-import About from './About/About';
-import Event from './Events/Event';
-import Footer from './footer/Footer';
-import Gallery from './Gallery/Gallery';
-import Header from './Header/Header';
-import HeroSection from './HeroSection/HeroSection';
-import MobileScroll from './MobileScroll/MobileScroll';
-import Team from './Team/Team';
-import WindowPeak from './WindowPeak';
+import React, { useState, useEffect } from "react";
+import About from "./About/About";
+import Event from "./Events/Event";
+import Footer from "./footer/Footer";
+import Gallery from "./Gallery/Gallery";
+import Header from "./Header/Header";
+import HeroSection from "./HeroSection/HeroSection";
+import MobileScroll from "./MobileScroll/MobileScroll";
+import Team from "./Team/Team";
+import WindowPeak from "./WindowPeak";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
-import ErrorPage from './Error/ErrorPage';
-import Contact from './contact/Contact';
-import Contactus from './contact/Contactus';
-import Massage from './mentorMassage/Massage';
-import Shows from './Shows/Shows';
-import PitchIdea from './PitchIdea/PitchIdea';
-import { Card } from '@chakra-ui/react';
-import TCard from './card/TCard';
+import ErrorPage from "./Error/ErrorPage";
+import Contact from "./contact/Contact";
+import Contactus from "./contact/Contactus";
+import Massage from "./mentorMassage/Massage";
+import Shows from "./Shows/Shows";
+import PitchIdea from "./PitchIdea/PitchIdea";
+import { Card } from "@chakra-ui/react";
+import TCard from "./card/TCard";
 // import Headersocials from './Header/Headersocials';
 
-
 function HomePage() {
-
   // const [loading, setLoading] = useState(false);
   // useEffect(() => {
   //   setLoading(true);
@@ -32,16 +30,16 @@ function HomePage() {
 
   return (
     <>
-      <HeroSection/>
-      <About/>
-      <Massage/>     
-      <Shows/>
+      <HeroSection />
+      <About />
+      <Massage />
+      <Shows />
       {/* <Event/> */}
       {/* <Team/> */}
       {/* <Gallery/> */}
       {/* <Contact/> */}
       {/* <Contactus/> */}
-      <PitchIdea/>
+      <PitchIdea />
       {/* <TCard/> */}
       {/* <Footer/> */}
       {/* <ErrorPage/> */}
