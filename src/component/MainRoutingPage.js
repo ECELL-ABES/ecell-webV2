96d33b3abeb3cc1677f5cc809c7ac36ead319c0a
Abhay <a.thakur01111@gmail.com>
2024-02-22 23:01:23 +0530

    achievement section v2

--- src/component/MainRoutingPage.js
+++ src/component/MainRoutingPage.js
@@ -1,73 +1,86 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes , Route, BrowserRouter} from 'react-router-dom';
-import Header from './pages/Header/Header';
-import HomePage from './pages/HomePage';
-import Event from './pages/Events/Event';
-import Team from './pages/Team/Team';
-import Gallery from './pages/Gallery/Gallery';
-import Contactus from './pages/contact/Contactus';
-import Footer from './pages/footer/Footer';
-import { useState, useEffect } from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Header from "./pages/Header/Header";
+import HomePage from "./pages/HomePage";
+import Event from "./pages/Events/Event";
+import Team from "./pages/Team/Team";
+import Gallery from "./pages/Gallery/Gallery";
+import Contactus from "./pages/contact/Contactus";
+import Footer from "./pages/footer/Footer";
+import { useState, useEffect } from "react";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
-import PastSpeaker from './pages/Past Speaker/PastSpeaker';
-import videoFile from './assets/bg/startup.gif'
-import EventDetails from './pages/Events/EventDetails/EventDetails';
-import ScrollToTop from './ScrollToTop';
-import Upload from './pages/Gallery/Upload';
+import PastSpeaker from "./pages/Past Speaker/PastSpeaker";
+import videoFile from "./assets/bg/startup.gif";
+import EventDetails from "./pages/Events/EventDetails/EventDetails";
+import ScrollToTop from "./ScrollToTop";
+import Upload from "./pages/Gallery/Upload";
 
 function MainRoutingPage() {
-
-    const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
+  //   const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 3000);
+  // }, []);
 
   return (
-<>
-    
- {
-      loading? 
-      <div style={{display: 'flex', 
-      justifyContent: 'center',
-       alignItems: 'center',
-       height:'100vh'}}>
-         <img src={videoFile} alt="" />
-         </div>
-      :
-    (
-    <Router>
-      <ScrollToTop/>
-    <Header/>
-    <Routes>
-    <Route path='/' exact element={<><HomePage/>
-    </>} />
-    
-    
-    <Route path='/events' exact element={<><Event/>
-    </>} />
-    
-    <Route path='/meetourteam' exact element={<><Team/>
-    </>} />
-    
-    <Route path='/gallery' exact element={<><Gallery/>
-    </>} />
-    {/* <Route path='/domainuploadfiles' exact element={<><Upload/>
+    <>
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <HomePage />
+              </>
+            }
+          />
+          <Route
+            path="/events"
+            exact
+            element={
+              <>
+                <Event />
+              </>
+            }
+          />
+          <Route
+            path="/meetourteam"
+            exact
+            element={
+              <>
+                <Team />
+              </>
+            }
+          />
+          <Route
+            path="/gallery"
+            exact
+            element={
+              <>
+                <Gallery />
+              </>
+            }
+          />
+          {/* <Route path='/domainuploadfiles' exact element={<><Upload/>
     </>} /> */}
-    
-   <Route path='/contactus' element={<Contactus/>} />
-   <Route path='/pastspeaker' element={<PastSpeaker/>} />     <Route path='/events/:id' element={<EventDetails/>} />
-   
-  </Routes>  
-   <Footer/>
-    </Router>
-    
-    )}
-</>
-  )
+          <Route path="/contactus" element={<Contactus />} />
+          <Route path="/pastspeaker" element={<PastSpeaker />} />{" "}
+          <Route path="/events/:id" element={<EventDetails />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
-export default MainRoutingPage
+export default MainRoutingPage;
